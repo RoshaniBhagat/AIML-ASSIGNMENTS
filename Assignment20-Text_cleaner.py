e7c56746d416bda98ef4787005eaec3baa116213
Roshani Bhagat <roshanibhagat0503@gmail.com>
2026-05-29 09:59:50 +0530

    Added Assignment files

--- Assignment20-Text_cleaner.py
+++ Assignment20-Text_cleaner.py
@@ -8,27 +8,26 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# Download required resources
 nltk.download('punkt')
 nltk.download('stopwords')
 
 def clean_text(text):
-    # 1. Convert to lowercase
+   
     text = text.lower()
 
-    # 2. Remove punctuation
+  
     text = text.translate(str.maketrans('', '', string.punctuation))
 
-    # 3. Tokenization
+   
     words = word_tokenize(text)
 
-    # 4. Remove stopwords
+    
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if word not in stop_words]
 
     return " ".join(words)
 
-# Test sentence
+
 sample_text = "Hello!!! This is a simple TEXT cleaning example."
 
 print("Original:", sample_text)
